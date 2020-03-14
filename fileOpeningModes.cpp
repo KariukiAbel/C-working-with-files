29423432af14db4b646780752b960be04cd3f2f9
KariukiAbel <abelmuriithi@gmail.com>
2020-03-14 23:12:44 +0300

    Used append mode to add text to the end fo file without moving the pointer

--- fileOpeningModes.cpp
+++ fileOpeningModes.cpp
@@ -3,18 +3,26 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  //fstream file;
+  fstream file;
   //using the constructor to cteate and open a file
-fstream file ("sample.txt", ios::out | ios::in);
+// fstream file ("sample.txt", ios::out | ios::in);
+
   // fstream default mode (flags) is
   // input output stream aka ios::in | ios::out
   //ios::in stands for INPUT used for reading
   //ios::out stands for OUTPUT used for writing
+  //ios::trunc stands for truncate
+  // ios::ate stands for At The End sets the ponter to the end of the File
+//ios::app appends data at the end of file
 
   // file.open("sample.txt", ios::out | ios::ate |ios::in);
+  file.open("sample.txt", ios::out | ios::app);
+
   //The line below creates and opens a new file called sample.txt
     // file.open("sample.txt", ios::out | ios::in);
   if (file.is_open()) {
+    file <<"sample text\n";
+    file <<"sample text\n";
   }else{
     std::cout << "File was not opened properly!" << '\n';
   }
